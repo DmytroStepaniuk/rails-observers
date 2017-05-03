ac4c3e16bc74a60990ad9a51493ceeda170b2e68
Dmytro Stepaniuk <stepaniuk@mlsdev.com>
2017-05-03 11:03:27 +0300

    railtie tiny update

--- lib/rails/observers/railtie.rb
+++ lib/rails/observers/railtie.rb
@@ -3,21 +3,21 @@ require 'rails/railtie'
 module Rails
   module Observers
     class Railtie < ::Rails::Railtie
-      initializer "active_record.observer", :before => "active_record.set_configs" do |app|
+      initializer 'active_record.observer', before: 'active_record.set_configs' do |app|
         ActiveSupport.on_load(:active_record) do
-          require "rails/observers/activerecord/active_record"
+          require 'rails/observers/activerecord/active_record'
           observers = app.config.active_record.delete(:observers)
           self.observers = observers if observers
         end
       end
 
-      initializer "action_controller.caching.sweepers" do
+      initializer 'action_controller.caching.sweepers' do
         ActiveSupport.on_load(:action_controller) do
-          require "rails/observers/action_controller/caching"
+          require 'rails/observers/action_controller/caching'
         end
       end
 
-      initializer "active_resource.observer" do |app|
+      initializer 'active_resource.observer' do |app|
         ActiveSupport.on_load(:active_resource) do
           require 'rails/observers/active_resource/observing'
 
