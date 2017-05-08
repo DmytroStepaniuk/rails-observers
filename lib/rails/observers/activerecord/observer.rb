2c7bea5c31024b7105a4e9a12e0f4cef48b4f10f
Dmytro Stepaniuk <stepaniuk@mlsdev.com>
2017-05-08 14:52:17 +0300

    add observed_callbacks method

--- lib/rails/observers/activerecord/observer.rb
+++ lib/rails/observers/activerecord/observer.rb
@@ -103,25 +103,30 @@ module ActiveRecord
 
       def add_observer!(klass)
         super
+
         define_callbacks klass
       end
 
-      def define_callbacks(klass)
+      def define_callbacks klass
         observer = self
+
         observer_name = observer.class.name.underscore.gsub('/', '__')
 
-        callbacks = ActiveRecord::Callbacks::CALLBACKS + [:after_create_commit, :after_update_commit, :after_destroy_commit]
+        observerd_callbacks.each do |callback|
+          next unless respond_to? callback
 
-        callbacks.each do |callback|
-          next unless respond_to?(callback)
-          callback_meth = :"_notify_#{observer_name}_for_#{callback}"
+          callback_meth = :"_notify_#{ observer_name }_for_#{ callback }"
+
           unless klass.respond_to?(callback_meth)
-            klass.send(:define_method, callback_meth) do |&block|
-              observer.update(callback, self, &block)
-            end
+            klass.send(:define_method, callback_meth) { |&block| observer.update(callback, self, &block) }
+
             klass.send(callback, callback_meth)
           end
         end
+      end
+
+      def observerd_callbacks
+        ActiveRecord::Callbacks::CALLBACKS + [:after_create_commit, :after_update_commit, :after_destroy_commit]
       end
   end
 end
