c85b395e81f81eb15f7154e38d8b7d0917f52d27
Dmytro Stepaniuk <stepaniuk@mlsdev.com>
2017-05-08 14:22:56 +0300

    get rid of useless sweepers stuff

--- lib/generators/active_record/observer/observer_generator.rb
+++ lib/generators/active_record/observer/observer_generator.rb
@@ -3,9 +3,9 @@ require 'rails/generators/active_record'
 module ActiveRecord
   module Generators
     class ObserverGenerator < Base
-      check_class_collision :suffix => "Observer"
+      check_class_collision suffix: 'Observer'
 
-      source_root File.expand_path("../templates", __FILE__)
+      source_root File.expand_path('../templates', __FILE__)
 
       def create_observer_file
         template 'observer.rb', File.join('app/models', class_path, "#{file_name}_observer.rb")
